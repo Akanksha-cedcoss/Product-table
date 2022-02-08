4b2fb6c46c2f367c860e9ef4c212a9f1869a197f
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-08 11:16:03 +0530

    real-final

--- src/script.js
+++ src/script.js
@@ -25,9 +25,8 @@ function submitForm() {
         price: price,
       };
       products.push(p);
-      display(p);
+      display();
     }
-    
   }
 }
 function NotduplicateId(id) {
@@ -39,13 +38,42 @@ function NotduplicateId(id) {
   }
   return true;
 }
-function display(object) {
-  var table = document.getElementById("table");
-  var row = table.insertRow(1);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  cell1.innerHTML = object.id;
-  cell2.innerHTML = object.name;
-  cell3.innerHTML = object.price;
+function display() {
+  var tblBody = document.createElement("tbody");
+  for (let i = 0; i < products.length; i++) {
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerText = products[i].id;
+    cell2.innerText = products[i].name;
+    cell3.innerText = products[i].price;
+    cell4.innerHTML =
+      '<a href="#" onclick="editForm(' + products[i].id + ')">Edit</a>';
+    tblBody.appendChild(row);
+  }
+  var old_tbody = document.getElementById("tbody");
+  if (old_tbody == null) {
+    document.getElementById("table").appendChild(tblBody);
+  } else {
+    document
+      .getElementById("tbody")
+      .parentNode.replaceChild(tblBody, old_tbody);
+  }
+
+  tblBody.setAttribute("id", "tbody");
 }
+function editForm(id) {
+  for (var i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      break;
+    }
+  }
+  document.getElementById("id").value = products[i].id;
+  document.getElementById("id").readOnly = true;
+  document.getElementById("id").style.border = "thick solid blue";
+  document.getElementById("id").value = products[i].id;
+  document.getElementById("id").value = products[i].id;
+}
+function update() {}
